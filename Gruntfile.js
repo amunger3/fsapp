ef1029f16f7860858e0c499ee0dce0f088080e4d
amunger3 <munger.alex@gmail.com>
2020-07-16 04:30:37 -0400

    scss init

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,8 @@ module.exports = function(grunt) {
                     { expand: true, cwd: 'node_modules/uikit/dist/css/', src: 'uikit.css', dest: 'assets/', filter: 'isFile' },
                     { expand: true, cwd: 'node_modules/uikit/dist/js/', src: 'uikit.js', dest: 'assets/', filter: 'isFile' },
                     { expand: true, cwd: 'node_modules/uikit/dist/js/', src: 'uikit-icons.js', dest: 'assets/', filter: 'isFile' },
+                    { expand: true, cwd: 'node_modules/uikit/src/scss/', src: '*', dest: 'resources/sass/uikit/', filter: 'isFile' },
+                    { expand: true, cwd: 'node_modules/uikit/src/js/', src: '*', dest: 'resources/js/uikit/', filter: 'isFile' },
                 ],
             },
         }
