e4588deb78625340f84551705af27d9f1ff2e203
Alex Munger <munger.alex@gmail.com>
2020-11-13 15:11:06 -0500

    upd runtimes

--- test_api_query.py
+++ test_api_query.py
@@ -2,7 +2,7 @@ from app.data.FBApi import FBDataHandler
 
 
 def test_filters():
-    filtered = FBDataHandler('BL', season='2019', stage='REGULAR_SEASON', status='FINISHED')
+    filtered = FBDataHandler('BL', season='2020', stage='REGULAR_SEASON', status='FINISHED')
     f_res = filtered.get_league_results()
     print(f_res['matches'][-1]['status'])
     assert f_res['matches'][-1]['status'] == 'FINISHED'
